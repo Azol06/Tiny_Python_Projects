134aec140723ba55b640e3665bf58155e67caf6a
Karina <karinaoliveira25050@gmail.com>
2026-03-27 19:03:55 -0300

    Introduction

--- guess.py
+++ guess.py
@@ -2,5 +2,7 @@ print("This is a guessing game," '\n')
 print("Guess the correct number to win!" '\n')
 
 import random
-print("Easy"   "Medium"   "Insane", sep="    " '\n')
+
+print("Easy",   "Medium",   "Insane", sep=" " '\n')
+
 level = input("Choose a dificulty to play to start: ")
