806ae8be0762a85ec821d61873a126d091802b63
Azol06 <karinaoliveira25050@gmail.com>
2026-04-01 09:53:00 -0300

    bug fix: retry_function now works

--- common_guess_game/functions.py
+++ common_guess_game/functions.py
@@ -1,3 +1,11 @@
-def retry():
-    retry = input("If you wanna play again type 'continue' or 'quit' to close the game: ")
-    return retry in ["continue", "yes", "y", "play"]
+def retry_function():
+    try:
+
+        retry = input("If you wanna play again type 'continue' or 'quit' to close the game: ")
+        if retry in ["continue", "yes", "y", "play"]:
+            return True
+                    
+        else: exit()
+    
+    except ValueError:
+        print("Something went wrong, please try again.")
--- common_guess_game/guess_game.py
+++ common_guess_game/guess_game.py
@@ -1,9 +1,10 @@
-from functions import retry
-import random
+from functions import retry_function #retry restart the game
+import random #random generates sort_number 
 
+coins = 100 #variables at alphabetical order
 space = "      "
-coins = 100
-easy_try = 20
+
+#attempts system
 normal_try = 15
 insane_try = 10
 
@@ -11,6 +12,7 @@ print("Welcome to the most generic guessing game you've ever seen!" "\n") #Title
 print("Easy" + space + "Normal" + space + "Insane")
 
 while True: #Game start
+    easy_try = 20
 
     try:
         level = input("Choose the difficulty level: ")
@@ -23,25 +25,33 @@ while True: #Game start
 
             sort_number = random.randint(1,100)
 
+
             for i in range (20): 
 
-                print(f"number: {sort_number}")
+                try:
+                    print(f"number: {sort_number}")
 
-                guess = int(input("Choose a number between 1 and 100: "))
+                    guess = int(input("Choose a number between 1 and 100: "))
 
-                if guess == sort_number:
-                    coins += 20
-                    print("Player Wins!")
-                    if retry():
-                        break
-                    else: exit()
-                else:
-                    easy_try -= 1
-                    if easy_try == 0:
-                        if retry():
+                    if guess == sort_number: #If player wins, he earn coins
+                        coins += 20
+                        print("Player Wins!")
+                        if retry_function ():
                             break
+                        else: exit()
+
+                    else: #else, loses his coins
+                        easy_try -= 1
+                        print(f"Your chances: {easy_try}")
+                        if easy_try <= 0:
+                            if retry_function():
+                                break
+                except ValueError:
+                    print("Something went wrong, please try again.")                
         else:
             print("Please type a valid value!")
+            continue
+
 
 
     except ValueError:
