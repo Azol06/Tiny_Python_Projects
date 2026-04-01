06efca4ca853449b03a075c3f6409163ff48a76e
Azol06 <karinaoliveira25050@gmail.com>
2026-04-01 16:17:35 -0300

    Final version, add coins, new levels and some bug fixes

--- common_guess_game/functions.py
+++ common_guess_game/functions.py
@@ -1,11 +1,43 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DB_PATH = os.path.join(BASE_DIR, "database.txt")
+
 def retry_function():
     try:
-
         retry = input("If you wanna play again type 'continue' or 'quit' to close the game: ")
         if retry in ["continue", "yes", "y", "play"]:
-            return True
-                    
+            return True          
         else: exit()
-    
     except ValueError:
         print("Something went wrong, please try again.")
+
+def guess_function(guess, sort_number):
+        if guess == sort_number: #If player wins, he earn coins
+            print("Player Wins!")
+            return True
+        elif guess > sort_number:
+            print("Try lower...")
+        else: print("Try higher...")
+        return False
+
+        
+def gameover_function(coins):
+        if coins <= 0:
+            print("Game Over, you are broke!")
+            return False
+        return True
+
+def load_coins ():
+    try:
+        with open(DB_PATH, "r") as archive:
+            return int(archive.read())
+    except:
+         return 100
+
+def save_coins(value):
+     with open(DB_PATH, "w") as archive:
+          archive.write(str(value))
+
+     
--- common_guess_game/guess_game.py
+++ common_guess_game/guess_game.py
@@ -1,64 +1,102 @@
 from functions import retry_function #retry restart the game
+from functions import guess_function
+from functions import gameover_function
+from functions import save_coins, load_coins
 import random #random generates sort_number 
 
-coins = 100 #variables at alphabetical order
+coins = load_coins()
 space = "      "
-
-#attempts system
-normal_try = 15
-insane_try = 10
 
 print("Welcome to the most generic guessing game you've ever seen!" "\n") #Title
 print("Easy" + space + "Normal" + space + "Insane")
 
 while True: #Game start
-    easy_try = 20
+    
+    if gameover_function(coins):
+        
+        try:
+            level = input("Choose the difficulty level: ")
+            level = str(level).lower()
+            print(f"Current Balance: {coins}")
+        except ValueError:
+            print("Something went wrong, please try again.")
+    
+        try:
+            if level in ["easy", "e", "1"]:
+                sort_number = random.randint(1, 100)
+                chances = 20
 
-    try:
-        level = input("Choose the difficulty level: ")
-        level = str(level).lower()
-    except ValueError:
-        print("Something went wrong, please try again.")
-
-    try:
-        if level in ["easy", "e", "1"]: #easy mode starts here
-
-            sort_number = random.randint(1,100)
-
-
-            for i in range (20): 
-
-                try:
-                    print(f"number: {sort_number}")
-
-                    guess = int(input("Choose a number between 1 and 100: "))
-
-                    if guess == sort_number: #If player wins, he earn coins
-                        coins += 20
-                        print("Player Wins!")
-                        if retry_function ():
-                            break
-                        else: exit()
-
-                    else: #else, loses his coins
-                        easy_try -= 1
-                        print(f"Your chances: {easy_try}")
-                        if easy_try <= 0:
+                for i in range(20): 
+                    try:
+                        guess = int(input(f"Chances: {chances} | Choose a number: "))
+                        if guess_function(guess, sort_number):
+                            coins += 20   
+                            save_coins(coins) 
                             if retry_function():
                                 break
-                except ValueError:
-                    print("Something went wrong, please try again.")                
-        else:
-            print("Please type a valid value!")
-            continue
+                            else: exit()
+                        else:
+                            chances -= 1
+                            print(f"Your chances: {chances}")
+                            if chances <= 0:
+                                coins -= 20
+                                save_coins(coins)
+                                if retry_function():
+                                    break
+                    except ValueError:
+                        print("Something went wrong, please try again.")
 
+            elif level in ["normal", "n", "2"]:
+                sort_number = random.randint(1, 100)
+                chances = 15
 
+                for i in range(15): 
+                    try:
+                        guess = int(input(f"Chances: {chances} | Choose a number: "))
+                        if guess_function(guess, sort_number):
+                            coins += 30   
+                            save_coins(coins) 
+                            if retry_function():
+                                break
+                            else: exit()
+                        else:
+                            chances -= 1
+                            print(f"Your chances: {chances}")
+                            if chances <= 0:
+                                coins -= 30
+                                save_coins(coins)
+                                if retry_function():
+                                    break
+                    except ValueError:
+                        print("Something went wrong, please try again.")
 
-    except ValueError:
-        print ("ValueError.")            
-    
+            elif level in ["insane", "i", "3"]:
+                sort_number = random.randint(1, 100)
+                chances = 10
 
-    
-        
+                for i in range(10): 
+                    try:
+                        guess = int(input(f"Chances: {chances} | Choose a number: "))
+                        if guess_function(guess, sort_number):
+                            coins += 40   
+                            save_coins(coins) 
+                            if retry_function():
+                                break
+                            else: exit()
+                        else:
+                            chances -= 1
+                            print(f"Your chances: {chances}")
+                            if chances <= 0:
+                                coins -= 40
+                                save_coins(coins)
+                                if retry_function():
+                                    break
+                    except ValueError:
+                        print("Something went wrong, please try again.")
 
+            else:
+                print("Invalid level! Please try again.")
+                continue
 
+        except ValueError:
+            print("ValueError.")
