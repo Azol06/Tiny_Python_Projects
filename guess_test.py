08f40dbcd82d571394e21fb15ac45aa597d77224
Azol06 <karinaoliveira25050@gmail.com>
2026-04-10 10:30:26 -0300

    Started matrix projects

--- guess_test.py
+++
@@ -1,8 +0,0 @@
-print("This is a guessing game," '\n')
-print("Guess the correct number to win!" '\n')
-
-import random
-
-print("Easy",   "Medium",   "Insane", sep=" " '\n')
-
-level = input("Choose a dificulty to play to start: ")
